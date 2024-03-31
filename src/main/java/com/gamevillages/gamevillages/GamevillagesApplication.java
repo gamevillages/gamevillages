aa2d3618637a50b06e7241c22d205a4b9c806106
dayounglee <verdantjuly@kakao.com>
2024-03-31 13:56:33 +0900

    chore : skeleton code

---
+++ src/main/java/com/gamevillages/gamevillages/GamevillagesApplication.java
@@ -0,0 +1,13 @@
+package com.gamevillages.gamevillages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamevillagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamevillagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamevillages/gamevillages/GamevillagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamevillages.gamevillages;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamevillagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
